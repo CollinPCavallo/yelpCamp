63646f136b6e1691115065c7d64b0c45e00e6c25
Collin Cavallo <collincavallo@gmail.com>
2018-07-04 19:17:45 -0500

    added restful routes(basic) and added mongoose for data persistance

--- app.js
+++ app.js
@@ -1,63 +1,87 @@
-var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
+var bodyParser = require('body-parser'),
+    express = require('express'),
+    app = express(),
+    mongoose = require('mongoose');
 
-    var campgrounds = [{
-            name: 'Salmon Creek',
-            image: 'https://www.fs.usda.gov/Internet/FSE_MEDIA/stelprdb5403963.jpg'
-        },
-        {
-            name: 'Buffalo Valley',
-            image: 'http://www.kalyumet.com/_upload/images/amenities/spacious-shaded-sites.jpg'
-        },
-        {
-            name: 'Buffalo Valley',
-            image: 'http://www.kalyumet.com/_upload/images/amenities/spacious-shaded-sites.jpg'
-        },
-        {
-            name: 'Buffalo Valley',
-            image: 'http://www.kalyumet.com/_upload/images/amenities/spacious-shaded-sites.jpg'
-        },
-        {
-            name: 'Buffalo Valley',
-            image: 'http://www.kalyumet.com/_upload/images/amenities/spacious-shaded-sites.jpg'
-        },
-        {
-            name: 'Buffalo Valley',
-            image: 'http://www.kalyumet.com/_upload/images/amenities/spacious-shaded-sites.jpg'
-        },
-        {
-            name: 'Galena Park',
-            image: 'https://www.arrivares.com/Image_Rep/Room_images/18412.jpg'
-        }
-    ];
+mongoose.connect('mongodb://localhost/yelp_camp');
+
+//Schema Setup
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create({
+//     name: 'Test',
+//     image: 'https://pixabay.com/get/eb31b70e2bf3073ed1584d05fb1d4e97e07ee3d21cac104496f0c678a6ecbdbc_340.jpg',
+//     description: 'this is a huge tent farm! Stop Abusing Tents'
+// },function(err, campground) {
+//     if(err) {
+//         console.log(err)
+//     } else {
+//         console.log('CAMPGROUND ADDED')
+//     }
+// });
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get('/', function (req, res) {
     res.render('landing')
 });
 
+//INDEX - GET Displays all campgrounds
 app.get('/campgrounds', function (req, res) {
-
-    
-    res.render('campgrounds', {campgrounds: campgrounds});
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('index', {campgrounds: allCampgrounds});
+        }
+    })
 });
 
-app.post('/campgrounds', function(req, res) {
-   var name = req.body.name;
-   var image = req.body.image;
-   var newCampground = {name: name, image: image};
-   campgrounds.push(newCampground);
-   res.redirect('/campgrounds');
+//CREATE - POST Add new campground to db
+app.post('/campgrounds', function (req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description
+    var newCampground = {name: name, image: image, description: description};
+    Campground.create(newCampground, function(err, newCreatedCampground) {
+        if(err) {
+            console.log(err)
+        } else {
+            res.redirect('/campgrounds');
+        }
+    })
 
 });
 
-app.get('/campgrounds/new', function(req, res) {
+//NEW - GET Displays for to make new campground.
+app.get('/campgrounds/new', function (req, res) {
     res.render('new');
 });
 
+//SHOW - GET Shows info about campground
+app.get('/campgrounds/:id', function (req, res) {
+    //Find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if(err) {
+            console.log(err)
+        } else {
+            //Render Show page for that campground
+            res.render('show' , {campground: foundCampground});
+        }
+    });
+    
+
+})
 app.listen(3000, function () {
     console.log('YelpCamp Server has started on port 3000')
 });
